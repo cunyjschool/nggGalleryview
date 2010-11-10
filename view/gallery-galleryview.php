d88a10f22736529a096c1e0038705a36bd3f4c3d
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-11-10 19:52:47 +0000

    Remove unncessary comment at the top

--- view/gallery-galleryview.php
+++ view/gallery-galleryview.php
@@ -1,16 +1,4 @@
 <?php 
-/**
-Template Page for the jQuery Galleryview integration
-
-Follow variables are useable :
-
-	$gallery     : Contain all about the gallery
-	$images      : Contain all images, path, title
-	$pagination  : Contain the pagination content
-
- You can check the content when you insert the tag <?php var_dump($variable) ?>
- If you would like to show the timestamp of the image ,you can use <?php echo $exif['created_timestamp'] ?>
-**/
 
 ?>
 <?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?><?php if (!empty ($gallery)) : ?>
